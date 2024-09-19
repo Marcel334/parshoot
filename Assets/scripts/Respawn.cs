019f2c2f7a9bf024f8a67989b40b22a4545e510e
Marcel334 <barystarygary@gmail.com>
2024-09-19 13:36:56 +0200

    2

--- Assets/scripts/Respawn.cs
+++ Assets/scripts/Respawn.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader1 : MonoBehaviour
 {
+    private void start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void LoadScene1()
     {
         SceneManager.LoadScene("Game");
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
     }
 }
